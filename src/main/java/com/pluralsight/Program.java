78ca61cb5ba5e144fef765361d92f7e0fe49ade8
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-25 17:51:05 -0500

    Feat: Bonus 1 inventory.csv

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,27 +1,56 @@
 package com.pluralsight;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Program {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        ArrayList<Product> inventory = getInventory();
-        System.out.println("We carry the following inventory: ");
+        ArrayList<Product> inventory = loadInventory();
+
 
         for(Product product : inventory) {
             System.out.printf("id: %d %s | Price: $%.2f%n", product.getId(), product.getName(), product.getPrice());
         }
 
     }
-    public static ArrayList<Product> getInventory() {
-        ArrayList<Product> inventory = new ArrayList<Product>();
+    public static ArrayList<Product> loadInventory() {
+        ArrayList<Product> inventory = new ArrayList<>();
 
-        inventory.add(new Product(1,"Pencil", 1.99));
-        inventory.add(new Product(2,"Eraser", 5.99));
-        inventory.add(new Product(3,"Notebook", 8.99));
-        inventory.add(new Product(4,"Pen", 2.99));
-        inventory.add(new Product(5,"Glue", 6.99));
+        System.out.print("What inventory would you like to see? ");
+        String userChoice = input.nextLine();
+        System.out.println("We carry the following inventory: ");
+
+
+        try {
+            FileReader fileReader = new FileReader(userChoice);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+
+            while(line != null) {
+                String[] parts = line.split("\\|");
+                int id = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                double price = Double.parseDouble(parts[2]);
+
+
+                inventory.add(new Product(id, name, price));
+                line = bufferedReader.readLine();
+
+
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong... File could not be loaded.");
+        }
 
 
         return inventory;
     }
 }
+
+
